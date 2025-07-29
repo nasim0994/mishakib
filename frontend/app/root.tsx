162b370e712bf4f829be3ef40338cb2fef772ac1
nasim0994 <nasimuddin0994@gmail.com>
2025-07-29 16:03:35 +0600

    completed seo

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -11,6 +11,60 @@ import "./app.css";
 import Providers from "./providers/Providers";
 import { Toaster } from "react-hot-toast";
 
+export async function loader() {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/seo`);
+  const seo = await res.json();
+  return seo;
+}
+
+export function meta({ matches }: Route.MetaArgs) {
+  const rootData = matches?.find((m) => m?.id === "root");
+  const seo = rootData?.data?.data;
+
+  return [
+    { title: seo?.title || "Root" },
+    {
+      name: "description",
+      content: seo?.description || "Welcome to React Router!",
+    },
+    {
+      name: "keywords",
+      content: seo?.keywords || "eBook",
+    },
+    {
+      name: "author",
+      content: seo?.author || "Nasim Uddin",
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1.0",
+    },
+    {
+      name: "copyright",
+      content: seo?.copyright || "Nasim Uddin",
+    },
+
+    // og tags
+    { property: "og:title", content: seo?.ogTitle || "Root" },
+    {
+      property: "og:description",
+      content: seo?.ogDescription || "Welcome to React Router!",
+    },
+    {
+      property: "og:image",
+      content: seo?.ogImageUrl || "",
+    },
+    {
+      property: "og:url",
+      content: seo?.ogUrl || "https://example.com",
+    },
+    {
+      property: "og:type",
+      content: seo?.ogType || "website",
+    },
+  ];
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
